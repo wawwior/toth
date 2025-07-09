b5ca476e88c7791034a4ca69e4b6d8570ada6035
wawwior <45405580+wawwior@users.noreply.github.com>
2025-07-09 16:06:22 +0200

    refactor: nested tests

--- src/test/java/me/wawwior/toth/json/JsonWriterTest.java
+++ src/test/java/me/wawwior/toth/json/JsonWriterTest.java
@@ -1,189 +1,258 @@
 package me.wawwior.toth.json;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.StringWriter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JsonWriterTest {
 
-    /**
-     * Compact style {@link JsonWriter} for testing.
-     * @param writer the backing {@link StringWriter}
-     * @return a JsonWriter
-     */
-    JsonWriter compact(StringWriter writer) {
-        return new JsonWriter(writer, JsonWriter.Style.compact());
+    interface ThrowingConsumer<T> {
+        void accept(T t) throws IOException;
     }
 
     /**
-     * Pretty style {@link JsonWriter} for testing.
+     * Utility method for creating tests.
+     *
+     * @param consumer the write operation
+     * @param expected the expected result
+     * @throws IOException If this happens, the test fails.
+     */
+    void compactTest(ThrowingConsumer<JsonWriter> consumer, String expected) throws IOException {
+
+        StringWriter stringWriter = new StringWriter();
+        consumer.accept(new JsonWriter(stringWriter, JsonWriter.Style.compact()));
+
+        assertEquals(expected, stringWriter.toString());
+    }
+
+    /**
+     * Utility method for creating tests.
      * <p>
-     * The indent is set as 2 spaces.
-     * @param writer the backing {@link StringWriter}
-     * @return a JsonWriter with 2 space indent
+     * The indent is set to 2 spaces
+     *
+     * @param consumer the write operation
+     * @param expected the expected result
+     * @throws IOException If this happens, the test fails.
      */
-    JsonWriter pretty(StringWriter writer) {
-        return new JsonWriter(writer, JsonWriter.Style.pretty("  "));
-    }
+    void prettyTest(ThrowingConsumer<JsonWriter> consumer, String expected) throws IOException {
 
-    /**
-     * Tests writing an empty json object with compact style.
-     * @throws IOException If this happens, the test fails
-     */
-    @Test
-    void writeEmpty_object_compact() throws IOException {
         StringWriter stringWriter = new StringWriter();
-        compact(stringWriter)
-                .openObject()
-                .closeObject();
-
-        //language=JSON
-        String expected = """
-                {}""";
+        consumer.accept(new JsonWriter(stringWriter, JsonWriter.Style.pretty("  ")));
 
         assertEquals(expected, stringWriter.toString());
     }
 
     /**
-     * Tests writing a json object with a key-value pair with compact style.
-     * @throws IOException If this happens, the test fails
+     * Tests writing an object to a {@link JsonWriter}...
      */
-    @Test
-    void write_object_compact() throws IOException {
-        StringWriter stringWriter = new StringWriter();
-        compact(stringWriter)
-                .openObject()
-                .name("key")
-                .value("value")
-                .closeObject();
+    @Nested
+    class ObjectTests {
 
-        //language=JSON
-        String expected = """
-                {"key":"value"}""";
+        /**
+         * ...with {@link JsonWriter.Style#compact()}.
+         */
+        @Nested
+        class Compact {
 
-        assertEquals(expected, stringWriter.toString());
+            /**
+             * Tests writing an empty json object.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void empty() throws IOException {
+                compactTest(writer -> writer.openObject().closeObject(),
+                        //language=JSON
+                        """
+                        {}""");
+            }
+
+            /**
+             * Tests writing a json object with a key-value pair.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void basic() throws IOException {
+                compactTest(writer -> writer.openObject().name("key").value("value").closeObject(),
+                        //language=JSON
+                        """
+                        {"key":"value"}""");
+            }
+
+            /**
+             * Tests writing a json object with a nested object.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void nestedObject() throws IOException {
+                compactTest(writer -> writer.openObject().name("foo").openObject().name("bar").value(
+                                "baz").closeObject().closeObject(),
+                        //language=JSON
+                        """
+                        {"foo":{"bar":"baz"}}""");
+            }
+
+            /**
+             * Tests writing a json object with a nested array.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void nestedArray() throws IOException {
+                compactTest(
+                        writer -> writer.openObject().name("key").openArray().value("value").closeArray().closeObject(),
+                        //language=JSON
+                        """
+                        {"key":["value"]}""");
+            }
+
+        }
+
+        /**
+         * ...with {@link JsonWriter.Style#pretty(String)}.
+         */
+        @Nested
+        class Pretty {
+
+            /**
+             * Tests writing an empty json object.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void writeEmpty() throws IOException {
+                prettyTest(writer -> writer.openObject().closeObject(),
+                        //language=JSON
+                        """
+                        {}""");
+            }
+
+            /**
+             * Tests writing a json object with a key-value pair.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void write() throws IOException {
+                prettyTest(writer -> writer.openObject().name("key").value("value").closeObject(),
+                        //language=JSON
+                        """
+                        {
+                          "key": "value"
+                        }""");
+            }
+
+            /**
+             * Tests writing a json object with a nested object.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void nestedObject() throws IOException {
+                prettyTest(writer -> writer.openObject().name("foo").openObject().name("bar").value(
+                                "baz").closeObject().closeObject(),
+                        //language=JSON
+                        """
+                        {
+                          "foo": {
+                            "bar": "baz"
+                          }
+                        }""");
+            }
+
+            /**
+             * Tests writing a json object with a nested array.
+             *
+             * @throws IOException If this happens, the test fails.
+             */
+            @Test
+            void nestedArray() throws IOException {
+                prettyTest(
+                        writer -> writer.openObject().name("key").openArray().value("value").closeArray().closeObject(),
+                        //language=JSON
+                        """
+                        {
+                          "key": [
+                            "value"
+                          ]
+                        }""");
+            }
+
+        }
+
     }
 
-    /**
-     * Tests writing an empty json object with pretty style.
-     * @throws IOException If this happens, the test fails
-     */
-    @Test
-    void writeEmpty_object_pretty() throws IOException {
-        StringWriter stringWriter = new StringWriter();
-        pretty(stringWriter)
-                .openObject()
-                .closeObject();
+    @Nested
+    class ArrayTests {
 
-        //language=JSON
-        String expected = """
-                {}""";
+        @Nested
+        class Compact {
 
-        assertEquals(expected, stringWriter.toString());
+            /**
+             * Tests writing an empty json array.
+             *
+             * @throws IOException If this happens, the test fails
+             */
+            @Test
+            void writeEmpty() throws IOException {
+                compactTest(writer -> writer.openArray().closeArray(),
+                        //language=JSON
+                        """
+                        []""");
+            }
+
+            /**
+             * Tests writing a json array with a value.
+             *
+             * @throws IOException If this happens, the test fails
+             */
+            @Test
+            void write() throws IOException {
+                compactTest(writer -> writer.openArray().value("value").closeArray(),
+                        //language=JSON
+                        """
+                        ["value"]""");
+            }
+
+        }
+
+
+        @Nested
+        class Pretty {
+
+            /**
+             * Tests writing a json array with a value.
+             *
+             * @throws IOException If this happens, the test fails
+             */
+            @Test
+            void writeEmpty() throws IOException {
+                prettyTest(writer -> writer.openArray().closeArray(),
+                        //language=JSON
+                        """
+                        []""");
+            }
+
+            /**
+             * Tests writing a json array with a value.
+             *
+             * @throws IOException If this happens, the test fails
+             */
+            @Test
+            void write() throws IOException {
+                prettyTest(writer -> writer.openArray().value("value").closeArray(),
+                        //language=JSON
+                        """
+                        [
+                          "value"
+                        ]""");
+            }
+        }
     }
-
-    /**
-     * Tests writing a json object with a key-value pair with pretty style.
-     * @throws IOException If this happens, the test fails
-     */
-    @Test
-    void write_object_pretty() throws IOException {
-
-        StringWriter stringWriter = new StringWriter();
-        pretty(stringWriter)
-                .openObject()
-                .name("key")
-                .value("value")
-                .closeObject();
-
-        //language=JSON
-        String expected = """
-                {
-                  "key": "value"
-                }""";
-
-        assertEquals(expected, stringWriter.toString());
-    }
-
-    /**
-     * Tests writing an empty json array with compact style.
-     * @throws IOException If this happens, the test fails
-     */
-    @Test
-    void writeEmpty_array_compact() throws IOException {
-        StringWriter stringWriter = new StringWriter();
-        compact(stringWriter)
-                .openArray()
-                .closeArray();
-
-        //language=JSON
-        String expected = """
-                []""";
-
-        assertEquals(expected, stringWriter.toString());
-    }
-
-    /**
-     * Tests writing a json array with a key-value pair with compact style.
-     * @throws IOException If this happens, the test fails
-     */
-    @Test
-    void write_array_compact() throws IOException {
-        StringWriter stringWriter = new StringWriter();
-        compact(stringWriter)
-                .openArray()
-                .value("value")
-                .closeArray();
-
-        //language=JSON
-        String expected = """
-                ["value"]""";
-
-        assertEquals(expected, stringWriter.toString());
-    }
-
-    /**
-     * Tests writing an empty json array with pretty style.
-     * @throws IOException If this happens, the test fails
-     */
-    @Test
-    void writeEmpty_array_pretty() throws IOException {
-        StringWriter stringWriter = new StringWriter();
-        pretty(stringWriter)
-                .openArray()
-                .closeArray();
-
-        //language=JSON
-        String expected = """
-                []""";
-
-        assertEquals(expected, stringWriter.toString());
-    }
-
-    /**
-     * Tests writing a json array with a key-value pair with pretty style.
-     * @throws IOException If this happens, the test fails
-     */
-    @Test
-    void write_array_pretty() throws IOException {
-
-        StringWriter stringWriter = new StringWriter();
-        pretty(stringWriter)
-                .openArray()
-                .value("value")
-                .closeArray();
-
-
-        //language=JSON
-        String expected = """
-                [
-                  "value"
-                ]""";
-
-        assertEquals(expected, stringWriter.toString());
-    }
-
 }
