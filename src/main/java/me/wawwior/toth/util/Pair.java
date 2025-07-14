4fa49cbe9a9eddf5e73373fee2fba9033ae3d353
wawwior <45405580+wawwior@users.noreply.github.com>
2025-07-14 22:40:31 +0200

    feat(util): add consumer accepting methods to Pair.java

--- src/main/java/me/wawwior/toth/util/Pair.java
+++ src/main/java/me/wawwior/toth/util/Pair.java
@@ -1,9 +1,20 @@
 package me.wawwior.toth.util;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public record Pair<A, B>(A a, B b) {
 
     public static <A, B> Pair<A, B> of(A a, B b) {
         return new Pair<>(a, b);
+    }
+
+    public void consume(BiConsumer<A, B> consumer) {
+        consumer.accept(a, b);
+    }
+
+    public <E extends Exception> void catchingConsume(CatchingBiConsumer<A, B, E> consumer) throws E {
+        consumer.accept(a, b);
     }
 
 }
