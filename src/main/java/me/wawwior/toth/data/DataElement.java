09f3d2af92061505fdfb613ab7b76efec1b38b97
wawwior <45405580+wawwior@users.noreply.github.com>
2025-11-18 23:51:40 +0100

    feat: add data type names

--- src/main/java/me/wawwior/toth/data/DataElement.java
+++ src/main/java/me/wawwior/toth/data/DataElement.java
@@ -19,18 +19,23 @@ public abstract class DataElement {
 
     public static class Type<T extends DataElement> {
 
-        public static final Type<DataNull> NULL_TYPE = new Type<>(reader -> DataNull.INSTANCE);
+        public static final Type<DataNull> NULL_TYPE = new Type<>("null_type", reader -> {
+            reader.expectNull();
+            return DataNull.INSTANCE;
+        });
 
-        public static final Type<DataMap> MAP_TYPE = new Type<>(DataMap::read);
-        public static final Type<DataList> LIST_TYPE = new Type<>(DataList::read);
+        public static final Type<DataMap> MAP_TYPE = new Type<>("map_type", DataMap::read);
+        public static final Type<DataList> LIST_TYPE = new Type<>("list_type", DataList::read);
 
-        public static final Type<DataBoolean> BOOLEAN_TYPE = new Type<>(DataBoolean::read);
-        public static final Type<DataNumber> NUMBER_TYPE = new Type<>(DataNumber::read);
-        public static final Type<DataString> STRING_TYPE = new Type<>(DataString::read);
+        public static final Type<DataBoolean> BOOLEAN_TYPE = new Type<>("boolean_type", DataBoolean::read);
+        public static final Type<DataNumber> NUMBER_TYPE = new Type<>("number_type", DataNumber::read);
+        public static final Type<DataString> STRING_TYPE = new Type<>("string_type", DataString::read);
 
+        private final String name;
         private final CatchingFunction<DataReader, T, IOException> fromReader;
 
-        private Type(CatchingFunction<DataReader, T, IOException> fromReader) {
+        private Type(String name, CatchingFunction<DataReader, T, IOException> fromReader) {
+            this.name = name;
             this.fromReader = fromReader;
         }
 
@@ -46,6 +51,10 @@ public abstract class DataElement {
             return fromReader.apply(reader);
         }
 
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 
 }
