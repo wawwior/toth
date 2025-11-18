251e5a5c44a7f704c4d3c307a2fa1a16652ea296
wawwior <45405580+wawwior@users.noreply.github.com>
2025-11-18 23:43:46 +0100

    chore: add jetbrain annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.13.1")
 
+    compileOnly("org.jetbrains:annotations:26.0.2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
